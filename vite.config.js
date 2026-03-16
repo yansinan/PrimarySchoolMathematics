5fc0901fad1bd7d00e0f81d75102c995b61aac78
Dr <yansinan@163.com>
2026-03-16 09:04:55 -0700

    增加停止服务；适配docker环境

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,14 @@ const srcPath = path.resolve(__dirname, 'src')
 
 export default defineConfig({
     server: {
-        port: 1101,
+        port: 8080, // 指定开发服务器端口
+        host:true, // 允许外部访问
+        strictPort: true, // 如果端口被占用则直接退出，而不是尝试下一个可用端口
     },
+    watch:{
+        usePolling: true, // 使用轮询方式监听文件变化
+    },
+    allowHosts: true, // 允许所有主机访问
     resolve: {
         alias: {
             '@/': `${srcPath}/`,
